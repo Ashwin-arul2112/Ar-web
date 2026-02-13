08409a36eacbfcbe01e82e40fa51f773a6903825
Ashwin-arul2112 <ashwinarul2112@gmail.com>
2026-02-13 14:38:40 +0530

    setup -3

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import ARPage from "./pages/ARPage"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ar" element={<ARPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/ar" element={<ARPage />} />
+    </Routes>
   )
 }
 
